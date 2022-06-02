44a344bb6fb3b53b4149615b5a51f7021144e1dc
AylenPattuzzi <88353158+AylenPattuzzi@users.noreply.github.com>
2022-06-02 00:16:12 -0300

    tailwindcss

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div className=' '>
+            <h2>Formulario</h2>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./idex.html", "./src/**/*.{jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
